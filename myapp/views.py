77b417e9e759058d63711780c5a55b39353f67ff
sparsh kashyap <sparshkashyap655@gmail.com>
2024-07-16 19:20:56 +0530

    phele todo app hai

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
+
+def todo(request):
+    return render(request,"todo.html")
+
+def add_todo(request):
+    return render(request,"add_todo.html")
